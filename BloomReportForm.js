eec140b71250b78ffe992690560eb7d0915a9452
dpadam-macrosolutions <dpadam@macrosolutions.com>
2015-12-27 20:56:51 -0500

    The HTML file for the HAB form
    This is the form that has the form for the user to submit Algae Bloom
    information to EPA.

---
+++ BloomReportForm.js
@@ -0,0 +1,35 @@
+﻿/**
+This function will have the code to send email
+toemail: address to send the email to
+fromemail: senders email address
+msgBody: JSON message with information from the form fields
+*/
+function sendEmail(tomail, frommail, sub, msg) {
+    alert("Sending Email to " + tomail + " from " + frommail + " with subject " 
+        + sub + " and message " + msg);
+
+
+}
+
+
+
+$(document).ready(function () {
+    $("#submit_btn").click(function () {
+
+        //create JSON string from all the fields on the form
+        formJSON = "Test bloom report message";
+
+        toaddr = "dpadam@macrosolutions.com";
+        fromaddr = "damanpadam@gmail.com";
+        msg = formJSON;
+        d = new Date();
+        sub = "Bloom Report on " + d.toUTCString();
+
+        //send email
+        sendEmail(toaddr, fromaddr, sub, msg);
+    });
+
+
+
+});
+
